de3e4dcfda515ef18e4b2e2ff1371ed4383c5cfe
jianjun.dong <jianjun.dong@bitmain.com>
2019-01-17 14:53:55 +0800

    sitemap

--- knack/models.py
+++ knack/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from users.models import Profile
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Knack(models.Model):
@@ -27,6 +28,9 @@ class Knack(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('knack:knack_detail', kwargs={'knack_id': self.id})
 
     class Meta:
         verbose_name = '窍门'
