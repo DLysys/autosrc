84c2100c5fa442578708bf00654d0f0b0ae79a00
jianjun.dong <jianjun.dong@bitmain.com>
2019-01-29 17:05:16 +0800

    suit2

--- knack/apps.py
+++ knack/apps.py
@@ -4,6 +4,14 @@ from django.apps import AppConfig
 import os
 
 
+from suit.apps import DjangoSuitConfig
+
+
+class SuitConfig(DjangoSuitConfig):
+    # layout = 'horizontal'
+    layout = 'vertical'
+
+
 class KnackConfig(AppConfig):
     name = os.path.split(os.path.dirname(__file__))[-1]
     verbose_name = '窍门'
