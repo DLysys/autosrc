d8424d605c5f7e19dded608a400b1c73753ad113
jianjun.dong <jianjun.dong@bitmain.com>
2019-01-29 16:57:24 +0800

    suit2

--- knack/apps.py
+++ knack/apps.py
@@ -4,6 +4,12 @@ from django.apps import AppConfig
 import os
 
 
+from suit.apps import DjangoSuitConfig
+# layout这个参数决定你的网页是初始样式是垂直样式还是水平样式，可选参数为‘horizontal’或‘vertical’
+class SuitConfig(DjangoSuitConfig):
+    layout = 'horizontal'
+
+
 class KnackConfig(AppConfig):
     name = os.path.split(os.path.dirname(__file__))[-1]
     verbose_name = '窍门'
